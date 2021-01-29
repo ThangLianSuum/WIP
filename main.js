94ebd151fd85dcc6f3dbe351dc0c4cfdcfd346bd
ThangLianSuum <75762682+ThangLianSuum@users.noreply.github.com>
2021-01-29 14:30:40 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var car1 
+{
+    car1_width = 120;
+    car_height = 70;
+    car1_image = "https://postimg.cc/9rqYz9HM";
+    car1_x = 10;
+    car1_y = 10;
+}
+
+var car2 
+{
+    car2_width = 120;
+    car_height = 70;
+    car2_image = "https://postimg.cc/9rqYz9HM";
+    car2_x = 10;
+    car2_y = 100;
+}
+myCanvas 
+{
+    backgound_image = "https://postimg.cc/p9D2dqqZ";
+}
+
+function add() {
+    background_imgTag = new Image(); //defining a variable with a new image
+    background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+    background_imgTag.src = background_image;   // load image
+
+    car1_imgTag = new Image(); //defining a variable with a new image
+    car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
+    car1_imgTag.src = car1_image;   // load image
+
+}    
+
+function uploadBackground() {
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height );
+}
+
+function uploadcar1() {
+    ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height );
+}
+
+window.addEventListener("keydown", my_keydown);
